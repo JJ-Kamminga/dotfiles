e0dd074e31afed9224cb4051d49f0c0742b948d6
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2023-03-09 15:35:25 +0100

    macos defaults

--- install_macos.sh
+++ install_macos.sh
@@ -3,3 +3,6 @@ ln -svi ~/dotfiles/bash/.bash_profile ~
 
 # git
 bash git/set_gitconfig.sh
+
+# macos defaults
+source macos/macosdefaults.sh
---
+++ macos/macosdefaults.sh
@@ -0,0 +1,21 @@
+# Ask for the administrator password upfront
+sudo -v
+
+# Disable the sound effects on boot
+sudo nvram SystemAudioVolume=" "
+## set screen capture location
+mkdir -p ~/documents/afbeeldingen/screenshots
+defaults write com.apple.screencapture location -string "~/documents/afbeeldingen/screenshots"
+## Display full POSIX path as Finder window title
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+###############################################################################
+# Activity Monitor                                                            #
+###############################################################################
+# Visualize CPU usage in the Activity Monitor Dock icon
+defaults write com.apple.ActivityMonitor IconType -int 5
+# Show all processes in Activity Monitor
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+# Sort Activity Monitor results by CPU usage
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0
