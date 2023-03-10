ababf1979e5f1b2e3363258db48bacdf17d481b8
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2023-03-10 10:02:39 +0100

    install brew git zsh and oh-my-zsh

--- install_macos.sh
+++ install_macos.sh
@@ -1,8 +1,29 @@
+###############################################################################
+# Configs                                                                     #
+###############################################################################
 # bash
 ln -svi ~/dotfiles/bash/.bash_profile ~
+
+# zsh
+ln -svi ~/dotfiles/zsh/.zshrc ~
 
 # git
 bash git/set_gitconfig.sh
 
 # macos defaults
 source macos/macosdefaults.sh
+
+###############################################################################
+# Installations                                                               #
+###############################################################################
+# brew
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# add to path
+(echo; echo 'eval "$(/usr/local/bin/brew shellenv)"') >> /Users/jkamminga/.zprofile
+eval "$(/usr/local/bin/brew shellenv)"
+
+# git
+brew install git
+
+# oh-my-zsh
+source zsh/install_oh-my-zsh.sh
