9e6c7c537596b7dfd2fce9ae981563e9e9cb7a1c
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2025-07-21 07:55:10 +0200

    set fonts on ubuntu

--- ubuntu/ubuntudefaults.sh
+++ ubuntu/ubuntudefaults.sh
@@ -4,5 +4,10 @@
 sudo ubuntu-drivers install
 
 # Settings
-snap set core snapshots.automatic.retention=no 
+snap set core snapshots.automatic.retention=no
 
+# Fonts
+echo "setting fonts..."
+gsettings set org.gnome.desktop.interface font-name 'Noto sans'
+gsettings set org.gnome.desktop.interface document-font-name 'Noto sans'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace'
