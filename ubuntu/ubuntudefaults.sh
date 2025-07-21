b19287f3093518f12308ceab4208c135c5dbaa92
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2025-07-21 11:41:22 +0200

    fix in font names

--- ubuntu/ubuntudefaults.sh
+++ ubuntu/ubuntudefaults.sh
@@ -8,6 +8,6 @@ snap set core snapshots.automatic.retention=no
 
 # Fonts
 echo "setting fonts..."
-gsettings set org.gnome.desktop.interface font-name 'Noto sans'
-gsettings set org.gnome.desktop.interface document-font-name 'Noto sans'
+gsettings set org.gnome.desktop.interface font-name 'Noto Sans 10'
+gsettings set org.gnome.desktop.interface document-font-name 'Noto Sans 10'
 gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace'
