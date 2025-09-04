d9e655c3380e7f11fed52030763b812f2264dc4d
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2025-09-04 15:42:48 +0200

    nvim lazyvim config

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -80,9 +80,8 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 
-# lazyvim
-git clone https://github.com/LazyVim/starter ~/.config/nvim
-rm -rf ~/.config/nvim/.git
+# lazyvim fork
+ln -sf ~/dotfiles/config/nvim ~/.config
 
 # taskwarrior
 sudo apt-get install taskwarrior
