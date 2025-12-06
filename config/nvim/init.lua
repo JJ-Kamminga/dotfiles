9e7f3732a04cb6922fe79bcb44c33527eb96f290
Jakob Jan Kamminga <43784616+JJ-Kamminga@users.noreply.github.com>
2025-12-06 21:53:27 +0100

    organize nvim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -2,34 +2,3 @@ require("config.lazy")
 require("config")
 require("lsp")
 
--- buffer-scoped
-vim.opt.autoindent = true
-
--- window-scoped
-vim.opt.cursorline = true
-
--- global scope
-vim.opt.autowrite = true
-
--- todo: move these to set file
-vim.opt.guicursor = ""
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
-
-vim.opt.wrap = false
-
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
-
-vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 8
-
-vim.opt.updatetime = 50
-vim.opt.colorcolumn = "80"
--- config/nvim/lua/config/options.lua
+++ config/nvim/lua/config/options.lua
@@ -7,3 +7,30 @@ vim.opt.cursorline = true
 -- global scope
 vim.opt.autowrite = true
 vim.opt.clipboard = "x11"
+
+-- todo: move these to set file?
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.wrap = false
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+
+-- autocomplete
+-- prevent the built-in vim.lsp.completion autotrigger from selecting the first item
+vim.opt.completeopt = { "menuone", "noselect", "popup" } 
